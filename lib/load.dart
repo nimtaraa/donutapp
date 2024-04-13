b70b248bb3fc1c5d9f8d89585fd836f9c3f8b3b7
nimtaraa <nim20021127>
2024-04-13 21:46:17 +0530

    first commit

---
+++ lib/load.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class loadin extends StatefulWidget {
+  const loadin({super.key});
+
+  @override
+  State<loadin> createState() => __loadingState();
+}
+
+class __loadingState extends State<loadin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:donutapp/load.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(donut());
+
+}
+class donut extends StatelessWidget {
+  const donut({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "SweetWheel",
+    home: loadin(),
+    );
+    
+  }
+}
