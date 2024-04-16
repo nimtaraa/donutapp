577ffa7a9cd274655d3e19fc23ef529eda04a61b
nimtaraa <nim20021127>
2024-04-17 00:58:59 +0530

    bottomnavigationbar created

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,8 @@
+import 'package:donutapp/itemm.dart';
 import 'package:donutapp/listt.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class homepage extends StatefulWidget {
   const homepage({super.key});
@@ -56,77 +59,212 @@ class _homepageState extends State<homepage> {
               )
             ],
           ),
+          bottomNavigationBar: BottomNavigationBar(
+            selectedLabelStyle: TextStyle(color: Colors.pink,fontSize: 10),
+            selectedItemColor: Colors.pink,
+            unselectedLabelStyle: TextStyle(color: Colors.grey,fontSize: 10),
+            unselectedItemColor: Colors.grey,
+            items: [
+            BottomNavigationBarItem(
+              
+              icon: IconButton(onPressed:() {
+              
+            }, icon:Icon(Icons.home,size: 20,)),label: "Home",),
+          
+           BottomNavigationBarItem(
+              
+              icon: IconButton(onPressed:() {
+              
+            }, icon:Icon(Icons.shopping_cart,size: 20,)),label: "Orders",),
+              BottomNavigationBarItem(
+              
+              icon: IconButton(onPressed:() {
+              
+            }, icon:Icon(Icons.card_giftcard,size: 20,)),label: "Rewards",),
+           BottomNavigationBarItem(
+              
+              icon: IconButton(onPressed:() {
+              
+            }, icon:Icon(Icons.person,size: 20,)),label: "Profile",)
+          ]
+          
+          
+          ),
       
       body: SingleChildScrollView(
-        child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,  
-      children: [
-         Padding(
-           padding: const EdgeInsets.only(left: 10,top: 20),
-           child: Row(
-            children: [
-              Text("NEW",style: TextStyle(color: Color.fromARGB(187, 0, 0, 0),fontWeight: FontWeight.bold,fontSize: 24),),
-              SizedBox(width: 10,),
-              Text("Pumpkin Spice donut!",style: TextStyle(color: Color.fromARGB(156, 0, 0, 0),fontSize: 18),)
-            ],
-           ),
-                
-
-         ),
-       Padding(
-         padding: const EdgeInsets.only(left: 10,top: 24),
-         child: Text("Featured",style: TextStyle(fontSize: 15,color: Colors.black,fontWeight: FontWeight.bold),),
-       ),
-       Padding(
-         padding: const EdgeInsets.only(top: 30),
-         child: Container(
-          height: 200,
-          width: double.infinity,
-           child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-             child: Padding(
-               padding: const EdgeInsets.only(left: 10),
-               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+        child: DefaultTabController(
+          length: 3,
+          child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,  
                 children: [
-                    listt(imagepath: 'sourcess/donut1.png', name: 'Pumpkin Spice', price: 'Rs 400.00',),
-                    
-                  SizedBox(width: 20,),
-                    listt(imagepath: 'sourcess/donut2.png', name: 'strawberry Lemon', price: 'Rs 450.00',),
-         
-               
-                                SizedBox(width: 20,),
-               
-                                    listt(imagepath: 'sourcess/donut3.png', name: 'strawberry ', price: 'Rs 350.00',),
-         
-                                SizedBox(width: 20,),
-               
-                                  listt(imagepath: 'sourcess/donut4.png', name: 'Vanila ', price: 'Rs 250.00',),
-         
-                                SizedBox(width: 20,),
-               
-                                  listt(imagepath: 'sourcess/donut5.png', name: 'Blueberry', price: 'Rs 750.00',),
-         
-                                SizedBox(width: 20,),
-               
-                                                    listt(imagepath: 'sourcess/donut6.png', name: 'Vanila Lemon', price: 'Rs 650.00',),
-
-                              SizedBox(width: 20,),
-               
+           Padding(
+             padding: const EdgeInsets.only(left: 10,top: 20),
+             child: Row(
+              children: [
+                Text("NEW",style: TextStyle(color: Color.fromARGB(187, 0, 0, 0),fontWeight: FontWeight.bold,fontSize: 24),),
+                SizedBox(width: 10,),
+                Text("Pumpkin Spice donut!",style: TextStyle(color: Color.fromARGB(156, 0, 0, 0),fontSize: 18),)
+              ],
+             ),
+                  
+          
+           ),
+                 Padding(
+           padding: const EdgeInsets.only(left: 10,top: 24),
+           child: Text("Featured",style: TextStyle(fontSize: 15,color: Colors.black,fontWeight: FontWeight.bold),),
+                 ),
+                 Padding(
+           padding: const EdgeInsets.only(top: 30),
+           child: Container(
+            height: 200,
+            width: double.infinity,
+             child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+               child: Padding(
+                 padding: const EdgeInsets.only(left: 10),
+                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                      listt(imagepath: 'sourcess/donut1.png', name: 'Pumpkin Spice', price: 'Rs 400.00',),
+                      
+                    SizedBox(width: 20,),
+                      listt(imagepath: 'sourcess/donut2.png', name: 'strawberry Lemon', price: 'Rs 450.00',),
+           
                  
-                ],
+                                  SizedBox(width: 20,),
+                 
+                                      listt(imagepath: 'sourcess/donut3.png', name: 'strawberry ', price: 'Rs 350.00',),
+           
+                                  SizedBox(width: 20,),
+                 
+                                    listt(imagepath: 'sourcess/donut4.png', name: 'Vanila ', price: 'Rs 250.00',),
+           
+                                  SizedBox(width: 20,),
+                 
+                                    listt(imagepath: 'sourcess/donut5.png', name: 'Blueberry', price: 'Rs 750.00',),
+           
+                                  SizedBox(width: 20,),
+                 
+                                                      listt(imagepath: 'sourcess/donut6.png', name: 'Vanila Lemon', price: 'Rs 650.00',),
+          
+                                SizedBox(width: 20,),
+                 
+                   
+                  ],
+                 ),
                ),
              ),
            ),
-         ),
-       ),
+                 ),
+          
+                Padding(
+          padding: const EdgeInsets.only(left: 8,top: 22,bottom: 15),
+          child: Text("Categories",style: TextStyle(fontSize: 15 ,fontWeight: FontWeight.bold,color: Colors.black),),
+                ),
+                TabBar(
+                  indicator: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14),
+                    color: Colors.pink
+                  ),
+                 labelColor: Colors.white,
+                 unselectedLabelColor: Colors.black,
+                  
+                  tabs: [
+                 
+                 Container(
+                  width: 400,
+                  decoration: BoxDecoration(
+                                        border: Border.all(width: 1,color: Colors.black),
 
-      Padding(
-        padding: const EdgeInsets.only(left: 8,top: 22),
-        child: Text("Categories",style: TextStyle(fontSize: 15 ,fontWeight: FontWeight.bold,color: Colors.black),),
-      ),
-      DefaultTabController(length: 3, child: )
-      ],
+                    borderRadius: BorderRadius.circular(15),),
+                  child: Center(child: Padding(
+                    padding: const EdgeInsets.only(top: 7,bottom: 7,left: 7,right: 7),
+                    child: Text("Hot drinks",style: TextStyle(fontSize: 12),),
+                  )),
+                 ),
+
+                 Container(
+                  width: 400,
+                  decoration: BoxDecoration(
+                                        border: Border.all(width: 1,color: Colors.black),
+
+                    borderRadius: BorderRadius.circular(15)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 7,left: 7,right: 7,bottom: 7),
+                    child: Center(child: Text("Iced drinks",style: TextStyle(fontSize: 12),)),
+                  ),
+                 ),
+
+                 Container(
+                  width: 400,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1,color: Colors.black),
+                    borderRadius: BorderRadius.circular(15)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 7,left: 7,right: 7,bottom: 7),
+                    child: Center(child: Text("Donuts",style: TextStyle(fontSize: 12),)),
+                  ),
+                 ),
+
+                ],
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  height: 300,
+                  child: TabBarView(children: [
+                    Column(
+                      children: [
+                        itemm(img: "sourcess/Cofee1.png", itemname: "Original Coffee"),
+                        itemm(img: "sourcess/Tea1.png", itemname: "Tea"),
+                        itemm(img: "sourcess/nespresso1.png", itemname: "Enespresso")
+
+
+                      ],
+                    ),
+
+                           Column(
+                      children: [
+                        itemm(img: "sourcess/Tea1(1).png", itemname: "Iced Latte"),
+                        itemm(img: "sourcess/Tea1(2).png", itemname: "Iced Macchiato"),
+                        itemm(img: "sourcess/Tea1(3).png", itemname: "Iced Coffee with milk")
+
+
+                      ],
+                    ),
+            SingleChildScrollView(
+              child: Column(
+                        children: [
+                          itemm(img: "sourcess/Cofee 1(2).png", itemname: "Tripple Chocolate"),
+              
+              
+              Container(
+                    child: Row(
+                      children: [
+                        SizedBox(width: 10,),
+                      Container(
+                        height: 110,
+                        width: 75,
+                        decoration: BoxDecoration(image: DecorationImage(image: AssetImage("sourcess/Tea 1(2).png"),fit: BoxFit.contain),),
+                        
+                      ),
+                      SizedBox(width: 10,),
+                      Text("Rasberry Vanilla"),
+                    ],),
+                  ),
+              
+              
+                          itemm(img: "sourcess/nespresso 1(2).png", itemname: "Classic Sprinkle")
+              
+              
+                        ],
+                      ),
+            ),
+            
+
+                ]))
+                
+                ],
+          ),
         ),
       ),
       
---
+++ lib/itemm.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class itemm extends StatefulWidget {
+  String img;
+  String itemname;
+
+   itemm({
+    required this.img,
+    required this.itemname,
+    super.key});
+
+  @override
+  State<itemm> createState() => _itemmState();
+}
+
+class _itemmState extends State<itemm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          SizedBox(width: 10,),
+        Container(
+          height: 75,
+          width: 75,
+          decoration: BoxDecoration(image: DecorationImage(image: AssetImage(widget.img),fit: BoxFit.contain),),
+          
+        ),
+        SizedBox(width: 10,),
+        Text(widget.itemname),
+      ],),
+    );
+  }
+}
